987dd40ea13e1b77b833da0ae7af1107f9bbbc33
vaibhavlohiya <vaibhavlohiya05@gmail.com>
2026-04-11 18:01:49 -0500

    Edititng the log command for the train_JetClass.sh

--- train_JetClass.sh
+++ train_JetClass.sh
@@ -116,6 +116,6 @@ $CMD \
     --model-prefix training/JetClass/${SAMPLE_TYPE}/${FEATURE_TYPE}/${model}/{auto}${suffix}/net \
     $dataopts $batchopts \
     --samples-per-epoch ${samples_per_epoch} --samples-per-epoch-val ${samples_per_epoch_val} --num-epochs $epochs --gpus 0 \
-    --optimizer ranger --log logs/JetClass_${SAMPLE_TYPE}_${FEATURE_TYPE}_${model}_{auto}${suffix}.log --predict-output pred.root \
+    --optimizer ranger --predict-output pred.root \
     --tensorboard JetClass_${SAMPLE_TYPE}_${FEATURE_TYPE}_${model}${suffix} \
     "${@:3}"
